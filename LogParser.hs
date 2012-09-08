4fa3c04d07d93cfa81314bd5065df58d23e434a6
Stefan Saasen <stefan@saasen.me>
2012-09-08 18:06:16 +1000

    First cut log parser

---
+++ LogParser.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
+
+module Main where
+
+import qualified Data.Attoparsec.Lazy as AL
+import Data.Attoparsec.Char8 hiding (space, take)
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
+import Control.Monad (liftM)
+import System.Environment (getArgs)
+import Prelude hiding (takeWhile)
+import qualified Data.HashMap.Strict as M
+import Data.List (sortBy, foldl')
+import Text.Printf (printf)
+import Data.Maybe (fromMaybe)
+import Data.ByteString.Char8 (readInteger)
+import Debug.Trace
+
+type Command = String
+
+-- REMOTE_ADRESS | PROTOCOL | (o|i)REQUEST_ID | USERNAME | date   URL | DETAILS | LABELS | TIME | SESSION_ID |
+-- REQUEST_ID -> MINUTE_OF_DAYxREQUEST_COUNTERxCONCURRENT_REQUESTS
+
+data InOurOut = In | Out deriving (Show, Eq)
+
+data RequestId = RequestId {
+     getInOrOut             :: Char
+    ,getRequestCounter      :: Integer
+    ,getConcurrentRequests  :: Integer
+} deriving (Show, Eq)
+
+data LogLine = LogLine {
+     getRemoteAdress        :: S.ByteString
+    ,getProtocol            :: S.ByteString
+    ,getRequestId           :: RequestId
+    ,getUsername            :: S.ByteString
+    ,getDate                :: S.ByteString
+    ,getAction              :: S.ByteString
+    ,getDetails             :: S.ByteString
+    ,getLabels              :: S.ByteString
+    ,getRequestDuration     :: S.ByteString
+    ,getSessionId           :: S.ByteString
+} deriving (Show, Eq)
+
+pipe, space, dash, quote, x :: Parser Char
+pipe        = satisfy (== '|')
+space       = satisfy (== ' ')
+dash        = satisfy (== '-')
+quote       = satisfy (== '\"')
+x           = satisfy (== 'x')
+
+quotedValue :: Parser S.ByteString
+quotedValue = do
+    quote
+    res <- takeTill (== '\"')
+    quote
+    return res
+
+logEntry :: Parser S.ByteString
+logEntry = do
+   entry <- takeTill (== '|')
+   {-trace("logEntry: " ++ show entry) pipe-}
+   pipe
+   space
+   return entry
+
+-- | i8x1401519x6 |
+parseRequestId :: Parser RequestId
+parseRequestId = do
+    which <- satisfy (\x -> x == 'i' || x == 'o')
+    minutes <- takeTill (== 'x')
+    _ <- x
+    counter <- takeTill (== 'x')
+    _ <- x
+    concurrent <- takeTill (== ' ')
+    space
+    pipe
+    space
+    return $ RequestId which (maybe 0 fst $ readInteger counter) (maybe 0 fst $ readInteger concurrent)
+
+    
+
+
+{-
+
+- 63.246.22.198,172.16.3.45 | https | o16x1402216x7 | cbac-confluence-user | 2012-09-08 00:17:00,270 | "GET /scm/ATLASSIAN/confluence.git/info/refs HTTP/1.1" | "" "JGit/unknown" | - | 1506 | - | 
+
+-}
+line :: Parser LogLine
+line = do
+    remoteAddress <- logEntry
+    protocol <- logEntry
+    requestId <- parseRequestId
+    username <- logEntry
+    date <- logEntry
+    action <- logEntry
+    details <- logEntry
+    labels <- logEntry
+    duration <- logEntry
+    sessionId <- logEntry
+
+    return $ LogLine remoteAddress protocol requestId username date action details labels duration sessionId
+    --return $ trace("remoteAddress: " ++ show remoteAddress) $ LogLine remoteAddress protocol requestId username date action details labels duration sessionId
+
+{-countBytes :: [L.ByteString] -> Int-}
+{-countBytes = foldl' count 0-}
+    {-where-}
+        {-count acc l = case AL.maybeResult $ AL.parse line l of-}
+            {-Just x  -> (acc +) . maybe 0 fst . S.readInt . getBytes $ x-}
+            {-Nothing -> acc-}
+
+{-countIPs :: [L.ByteString] -> [(S.ByteString,Int)]-}
+{-countIPs = M.toList . foldl' count M.empty-}
+    {-where-}
+        {-count acc l = case AL.maybeResult $ AL.parse line l of-}
+            {-Just x  -> M.insertWith (+) (S.copy (getIP x)) 1 acc-}
+            {-Nothing -> acc-}
+
+countLines :: [L.ByteString] -> Integer
+countLines = foldl' count' 0
+    where
+        count' acc l = case AL.maybeResult $ AL.parse line l of
+            Just x  -> acc + 1
+            {-Just x  -> (acc +) . maybe 0 fst . S.readInt . getBytes $ x-}
+            Nothing -> trace("no parse result") acc
+
+
+pretty :: Show a => Int -> (a, Int) -> String
+pretty i (bs, n) = printf "%d: %s, %d" i (show bs) n
+
+---------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  [cmd,path] <- getArgs
+  dispatch cmd path
+
+dispatch :: Command -> FilePath -> IO ()
+dispatch cmd path = action path
+    where
+        action = fromMaybe err (lookup cmd actions)
+        err    = \_ -> putStrLn $ "Error: " ++ cmd ++ " is not a valid command."
+                    
+actions :: [(Command, FilePath -> IO ())]
+{-actions = [("bytes", countTotalBytes)-}
+          {-,("ips", mapToTopList countIPs)]-}
+actions = [("count", countLogFileLines)]
+
+countLogFileLines :: FilePath -> IO ()
+countLogFileLines path = print . countLines . L.lines =<< L.readFile path
+
+
+
+{-mapToTopList :: ([L.ByteString] -> [(S.ByteString, Int)]) -> FilePath -> IO ()-}
+{-mapToTopList f p = do-}
+    {-file <- liftM L.lines $ L.readFile p-}
+    {-let mostPopular (_,a) (_,b) = compare b a-}
+        {-m = f file-}
+    {-mapM_ putStrLn . zipWith pretty [1..] . take 10 . sortBy mostPopular $ m-}
