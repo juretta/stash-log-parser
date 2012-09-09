28e15d05772ded52d44bad5812cb9859555ab6d6
Stefan Saasen <stefan@saasen.me>
2012-09-09 21:01:43 +1000

    Run Hlint before building, add profiling option

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+set -e
+set -u
+
 IN="LogParser.hs"
 OUT="logparser"
 
-ghc -O2 --make $IN -o $OUT
+# Run hlint first
+hlint --cross --color --report --utf8 $IN
+
+#ghc -rtsopts --make $IN -o $OUT
+
+ghc -rtsopts -O2 --make $IN -o $OUT
+
+# With profiling
+#ghc -rtsopts -prof -O2 --make $IN -o $OUT
