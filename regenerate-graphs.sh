3755354ce2f6fd4ba592ae3538743c9c93949fae
Stefan Saasen <stefan@saasen.me>
2012-10-16 13:14:32 +1100

    Move the gnuplot scripts into the ./gnuplot directory

--- regenerate-graphs.sh
+++ regenerate-graphs.sh
@@ -6,8 +6,12 @@ set -u
 
 rm -f *.png
 
-time ./dist/build/logparser/logparser plotGitOperations ../access-logs/aggregated-log-file +RTS -sstderr > plot-git-ops
-gnuplot < generate-git-ops-plot.plot
+# Stash prod
+LOG_FILE="../access-logs/aggregated-log-file"
 
-time ./dist/build/logparser/logparser plotConnHour ../access-logs/aggregated-log-file +RTS -sstderr  > plot-all
-gnuplot < generate-max-conn-plot.plot
+
+time ./dist/build/logparser/logparser plotGitOperations ${LOG_FILE} +RTS -sstderr > plot-git-ops
+gnuplot < gnuplot/generate-git-ops-plot.plot
+
+time ./dist/build/logparser/logparser plotConnHour ${LOG_FILE} +RTS -sstderr  > plot-all
+gnuplot < gnuplot/generate-max-conn-plot.plot
