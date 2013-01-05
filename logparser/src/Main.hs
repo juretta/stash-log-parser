1728d0275b07ed00a04ae83d7d0e3e4d296edcaa
Stefan Saasen <stefan@saasen.me>
2013-01-05 13:14:12 +1100

    Move from UI.Command to CmdArgs

--- logparser/src/Main.hs
+++ logparser/src/Main.hs
@@ -1,86 +1,77 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, RecordWildCards #-}
 module Main where
 
-import Stash.Log.Analyser
+import Stash.Log.Analyser hiding (ProtocolStats)
 import Stash.Log.GitOpsAnalyser
 import Stash.Log.Output
-import Data.Default
-import UI.Command
+import System.Console.CmdArgs
 import Prelude hiding (takeWhile)
-import Control.Monad.Trans (liftIO)
 
 -- =================================================================================
 
-logparser :: Application () ()
-logparser = def {
-                appName = "logparser",
-                appVersion = "1.9",
-                appAuthors = ["Stefan Saasen"],
-                appBugEmail = "ssaasen@atlassian.com",
-                appCategories = ["Logfile analysis", "Debug"],
-                appShortDesc = "Logparser for the Atlassian Stash access logs",
-                appLongDesc = "Parses and aggregates the access logs of Atlassian Stash",
-                appCmds = [count, countRequests, maxConn, summarizeGitOperations, requestDurations, summarizeProtocolStats, debugParser ]
-        }
+appName :: String
+appName = "logparser"
+
+appVersion :: String
+appVersion = "1.9"
+
+appShortDesc :: String
+appShortDesc = "Logparser for the Atlassian Stash access logs"
+
+data LogParser = MaxConn {files :: [FilePath]}
+                | CountRequests {files :: [FilePath]}
+                | GitOperations {files :: [FilePath]}
+                | GitDurations {files :: [FilePath]}
+                | ProtocolStats {files :: [FilePath]}
+                | Count {files :: [FilePath]}
+                | DebugParser {files :: [FilePath]}
+             deriving (Data,Typeable,Show,Eq)
+
+maxConn :: LogParser
+maxConn         = MaxConn {files = def &= args}
+                &= name "maxConn"       &= help "Show the maximum number of concurrent requests per hour"
+
+countRequests :: LogParser
+countRequests   = CountRequests {files = def &= args}
+                &= name "countRequests" &= help "Count the number of requests"
+
+gitOperations :: LogParser
+gitOperations   = GitOperations {files = def &= args}
+                &= name "gitOperations" &= help "Aggregate git operations per hour. Show counts for fetch, clone, push, pull and ref advertisement"
+
+gitDurations :: LogParser
+gitDurations    = GitDurations {files = def &= args}
+                &= name "gitDurations"  &= help "Show the duration of git operations over time"
+
+protocolStats :: LogParser
+protocolStats   = ProtocolStats {files = def &= args}
+                &= name "protocolStats" &= help "Aggregate the number of git operations per hour based on the access protocol (http(s) vs. SSH)"
+
+count :: LogParser
+count           = Count {files = def &= args}
+                &= name "count"         &= help "Count the number of lines in the given logfile(s)"
+
+debugParser :: LogParser
+debugParser     = DebugParser {files = def &= args}
+                &= name "debugParser"   &= help "Parse and print the first five lines of the log file"
 
 
-count, countRequests, maxConn, summarizeGitOperations, requestDurations, summarizeProtocolStats, debugParser :: Command ()
+mode :: Mode (CmdArgs LogParser)
+mode = cmdArgsMode $ modes [maxConn, countRequests, gitOperations, gitDurations, protocolStats, count, debugParser]
+        &= help appShortDesc
+        &= program appName &= summary (appName ++ " " ++ appVersion)
 
-countRequests = defCmd {
-                cmdName = "countRequests",
-                cmdHandler = commandHandler $ parseAndPrint countRequestLines,
-                cmdCategory = "Logfile analysis",
-                cmdShortDesc = "Count the number of requests"
-        }
 
-maxConn = defCmd {
-                cmdName = "maxConn",
-                cmdHandler = commandHandler $ printPlotDataConcurrentConn plotDataConcurrentConnHour,
-                cmdCategory = "Logfile analysis",
-                cmdShortDesc = "Show the maximum number of concurrent requests per hour"
-        }
-
-summarizeGitOperations = defCmd {
-                cmdName = "gitOperations",
-                cmdHandler = commandHandler $ printPlotDataGitOps analyseGitOperations,
-                cmdCategory = "Logfile analysis",
-                cmdShortDesc = "Aggregate git operations per hour. Show counts for fetch, clone, push, pull and ref advertisement"
-        }
-
-requestDurations = defCmd {
-                cmdName = "gitDurations",
-                cmdHandler = commandHandler $ printGitRequestDurations gitRequestDuration,
-                cmdCategory = "Logfile analysis",
-                cmdShortDesc = "Show the duration of git operations over time"
-        }
-
-summarizeProtocolStats = defCmd {
-                cmdName = "protocolStats",
-                cmdHandler = commandHandler $ printProtocolData protocolStatsByHour,
-                cmdCategory = "Logfile analysis",
-                cmdShortDesc = "Aggregate the number of git operations per hour based on the access protocol (http(s) vs. SSH)"
-        }
-
-debugParser = defCmd {
-                cmdName = "debugParser",
-                cmdHandler = commandHandler $ parseAndPrint showLines,
-                cmdCategory = "Debug",
-                cmdShortDesc = "Parse and print the first five lines of the log file"
-        }
-
-count = defCmd {
-                cmdName = "count",
-                cmdHandler = commandHandler $ printCountLines countLines,
-                cmdCategory = "Debug",
-                cmdShortDesc = "Count the number of lines in the given logfile(s)"
-        }
-
-commandHandler f = do
-    args <- appArgs
-    case args of
-        []          -> error "Path to logfile(s) is missing"
-        files       -> liftIO $ f files
-
+run :: LogParser -> IO ()
+run (MaxConn files)         = printPlotDataConcurrentConn plotDataConcurrentConnHour files
+run (CountRequests files)   = parseAndPrint countRequestLines files
+run (GitOperations files)   = printPlotDataGitOps analyseGitOperations files
+run (GitDurations files)    = printGitRequestDurations gitRequestDuration files
+run (ProtocolStats files)   = printProtocolData protocolStatsByHour files
+run (Count files)           = printCountLines countLines files
+run (DebugParser files)     = parseAndPrint showLines files
 
 main :: IO ()
-main = appMain logparser
+main = do
+    config <- cmdArgsRun mode
+    run config
