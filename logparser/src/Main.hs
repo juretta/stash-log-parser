948c655ec94abb201ebc070a8e89756f41ccfcef
Stefan Saasen <stefan@saasen.me>
2013-08-26 10:24:56 +1000

    Bump version to 1.17

--- logparser/src/Main.hs
+++ logparser/src/Main.hs
@@ -16,7 +16,7 @@ appName :: String
 appName = "logparser"
 
 appVersion :: String
-appVersion = "1.16"
+appVersion = "1.17"
 
 appShortDesc :: String
 appShortDesc = "Logparser for the Atlassian Stash access logs"
